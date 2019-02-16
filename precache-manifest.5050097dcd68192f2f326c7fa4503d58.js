c033b964e83e65db6de48a81959525da896a85a8
Rex Ogbemudia <rex.ogbemudia@andela.com>
2019-02-16 09:31:26 +0100

    Updates

---
+++ precache-manifest.5050097dcd68192f2f326c7fa4503d58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8854a6ac48eeb641b72",
+    "url": "/Ideal-pool/static/js/runtime~main.b8854a6a.js"
+  },
+  {
+    "revision": "5e02a8d7e4e92a7c89df",
+    "url": "/Ideal-pool/static/js/main.5e02a8d7.chunk.js"
+  },
+  {
+    "revision": "8858d9fe703d52aec4e1",
+    "url": "/Ideal-pool/static/js/1.8858d9fe.chunk.js"
+  },
+  {
+    "revision": "5e02a8d7e4e92a7c89df",
+    "url": "/Ideal-pool/static/css/main.e014978e.chunk.css"
+  },
+  {
+    "revision": "8858d9fe703d52aec4e1",
+    "url": "/Ideal-pool/static/css/1.68ca686a.chunk.css"
+  },
+  {
+    "revision": "970be6c5b2cc8df092ae180ee1c36a38",
+    "url": "/Ideal-pool/index.html"
+  }
+];
